c9109e7d21d50e9b9f54d78657fa7943070a9d00
luhluh-17 <castro.ralph17@gmail.com>
2022-05-21 03:05:41 +0800

    adds navbar color toggle when scrolling

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ function toggleTheme() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+window.addEventListener("scroll", function (e) {
+  var nav = document.getElementById("nav-bar");
+  if (
+    document.documentElement.scrollTop ||
+    document.body.scrollTop > window.innerHeight
+  ) {
+    nav.classList.add("nav-colored");
+    nav.classList.remove("nav-transparent");
+  } else {
+    nav.classList.add("nav-transparent");
+    nav.classList.remove("nav-colored");
+  }
+});
